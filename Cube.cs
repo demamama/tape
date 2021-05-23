06a037cdff4bff70950a03eddffb7b10996950c0
demamama <82538255+demamama@users.noreply.github.com>
2021-05-23 22:39:06 +0300

    Add files via upload

---
+++ Cube.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.TryGetComponent(out Tape tape))
+        {
+            transform.position += Vector3.forward * _speed * Time.deltaTime;
+        }
+    }
+}
